d35137f87df8d14b86e3ed17550e7fc930ddadbc
김세현 <93258358+tkatpgus19@users.noreply.github.com>
2024-07-12 16:34:29 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,19 +11,22 @@ public class Main {
         int B = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        int plus = A-B;
-        int day = V/plus + 1;
+        // 마지막 날은 무조건 오르므로 미리 계산
+        int day = 1;
+        int len = V - A;
 
-        int height = plus * day;
-
-        while(true) {
-            height += B;
-            if(height < V) {
-                break;
+        // 그래도 높이가 남는다면 추가되는 날 계산
+        if(len > 0){
+            // 하루에 올라갈 수 있는 최대 높이로 나누어 걸리는 일 수를 계산
+            if(len % (A-B) == 0){
+                day += len / (A-B);
             }
-            height -= A;
-            day--;
+            // 완벽히 나누어 떨어지지 않으면 하루가 더 필요함
+            else{
+                day += len / (A-B) + 1;
+            }
         }
-        System.out.println(day+1);
+
+        System.out.println(day);
     }
 }
