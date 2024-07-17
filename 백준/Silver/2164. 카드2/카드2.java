5d6bb015e5f462f336095b4c9391287f08a20c55
김세현 <93258358+tkatpgus19@users.noreply.github.com>
2024-07-17 15:22:03 +0900

    [Silver IV] Title: 카드2, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,22 +5,27 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Deque<Integer> queue = new ArrayDeque<>();
-
         int N = Integer.parseInt(br.readLine());
-        for(int i=1; i<N+1; i++) {
-            queue.addLast(i);
+
+        int num = 1;
+
+        if(N == 1) {
+            System.out.println(1);
+            return;
         }
 
-        int answer = 0;
-        while(true) {
-            answer = queue.removeFirst();
-            if(queue.isEmpty()){
-                break;
-            }
-            queue.addLast(queue.removeFirst());
+        while(num < N) {
+            num *= 2;
         }
-        System.out.println(answer);
+        num /= 2;
+        System.out.println((N-num)*2);
 
+
+        // 1,
+        // 2,
+        // 2,4,
+        // 2,4,6,8,
+        // 2,4,6,8,10,12,14,16,
+        // 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32
     }
 }
