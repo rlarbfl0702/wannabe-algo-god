4864fa01b4341f1c7440fb844a1733571f0bea61
김세현 <93258358+tkatpgus19@users.noreply.github.com>
2024-09-05 23:19:52 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|\n");
+        System.out.println(sb);
+    }
+}
