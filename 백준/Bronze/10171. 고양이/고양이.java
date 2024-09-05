4b0b831a29e07e6a368037c14ba6fe1ee0eb02c9
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2024-09-05 21:15:27 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
