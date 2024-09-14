1c8606cee7969f1c68b2080ae54ffb7ac1921638
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2024-09-14 23:56:05 +0900

    [Bronze III] Title: 오븐 시계, Time: 120 ms, Memory: 15984 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int plus = Integer.parseInt(br.readLine());
+
+        M = M + plus;
+
+        H = H +  M / 60;
+        M = M % 60;
+
+        H = H % 24;
+
+        System.out.println(H + " " + M);
+    }
+}
