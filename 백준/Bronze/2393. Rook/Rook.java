65b6d15e3bfa5fc43090db8a125580f3ba43d228
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2024-12-30 23:22:00 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/ \n    \\_______/ \n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\ \n/_______________\\ \n");
+    }
+}
