ec02994a093981c07701d9c5ecbbc6b958d3b9d9
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-01-16 18:39:46 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 64 ms, Memory: 11420 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+s+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
