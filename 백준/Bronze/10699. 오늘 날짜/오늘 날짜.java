552959a8ae8c788a2acdfcffff79859662511c01
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-02-03 20:09:31 +0900

    [Bronze V] Title: 오늘 날짜, Time: 92 ms, Memory: 13128 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LocalDate time = LocalDate.now();
+
+        System.out.println(time);
+    }
+}
