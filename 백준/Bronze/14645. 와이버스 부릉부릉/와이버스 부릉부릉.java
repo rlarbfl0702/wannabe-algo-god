78d80d973a1ff98db87b80f927754c93100a79e8
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-02-04 20:40:51 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 64 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+        }
+
+        System.out.println("비와이");
+    }
+}
