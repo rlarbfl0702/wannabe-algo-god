67af1b608cb3bffd03c5dc2b3bbfdd12e6f2bd1b
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-02-13 21:48:36 +0900

    [Bronze V] Title: 등록, Time: 60 ms, Memory: 11444 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(260);
+        System.out.println("prote0702");
+    }
+}
