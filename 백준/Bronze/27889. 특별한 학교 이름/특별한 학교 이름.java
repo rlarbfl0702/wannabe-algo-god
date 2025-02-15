599cfea52f939709de9c42249f4b2e7e8ee34250
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-02-15 23:22:21 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        if(s.equals("NLCS")){
+            System.out.println("North London Collegiate School");
+        }
+        else if(s.equals("BHA")){
+            System.out.println("Branksome Hall Asia");
+        }
+        else if(s.equals("KIS")){
+            System.out.println("Korea International School");
+        }
+        else if(s.equals("SJA")){
+            System.out.println("St. Johnsbury Academy");
+        }
+    }
+}
