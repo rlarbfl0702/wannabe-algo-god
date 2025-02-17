847d0dd7c706025a09072ef5416edc4783834970
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-02-18 03:14:39 +0900

    [Bronze V] Title: 팩토리얼 3, Time: 2948 ms, Memory: 365976 KB -BaekjoonHub

---
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(1, N));
+    }
+
+    public static BigInteger factorial(int a, int n){
+        BigInteger num = BigInteger.valueOf(a);
+
+        // a가 n보다 작으면
+        if (a < n){
+            // a와 n의 중간값을 b에 할당
+            int b = (a + n) / 2;
+            // 재귀함수 사용 > a부터 b까지 계산 > b+1 부터 n까지 계산
+            // 그냥 a부터 n까지 재귀로 계산한다고 보면 됨
+            num = factorial(a,b).multiply(factorial(b+1, n));
+        }
+        return num;
+    }
+}
