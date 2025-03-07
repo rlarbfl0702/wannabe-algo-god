9e662a1b30ca4548a12155d54df309a11a1f9cf3
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-03-07 17:26:08 +0900

    [Silver I] Title: 오목, Time: 68 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -1,81 +1,118 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static int[][] map;
-    public static int[] dx = {0, 1, 1, -1}; // 오른쪽, 아래, 우하향 대각선, 좌하향 대각선
-    public static int[] dy = {1, 0, 1, 1}; // 4방향만 검사 (반대 방향은 검사하지 않음)
+public class Main{
+
+    public static int map[][];
+    public static int dx[] = {0, 1, 1, -1};
+    public static int dy[] = {1, 0, 1, 1};
+    public static ArrayList<Node> start;
+
+    public static class Node{
+        int x;
+        int y;
+
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         map = new int[19][19];
+        start = new ArrayList<>();
 
-        for (int i = 0; i < 19; i++) {
+        for(int i = 0; i < 19; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 19; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
 
-        for (int i = 0; i < 19; i++) {
-            for (int j = 0; j < 19; j++) {
-                if (map[i][j] != 0) {
-                    int winner = map[i][j];
-                    for (int d = 0; d < 4; d++) { // 4방향만 검사
-                        if (checkWin(i, j, d, winner)) {
-                            System.out.println(winner);
-                            System.out.println((i + 1) + " " + (j + 1));
-                            return;
-                        }
-                    }
+            for(int j = 0; j < 19; j++){
+                int n = Integer.parseInt(st.nextToken());
+
+                map[i][j] = n;
+
+                // 흰돌, 검은돌 시작 위치 지점을 저장
+                if(n != 0){
+                    start.add(new Node(i, j));
                 }
             }
         }
-        System.out.println(0);
+
+        boolean TF = false;
+
+        for(Node node : start){
+            int sx = node.x;
+            int sy = node.y;
+
+            for(int i = 0; i < 4; i++){
+                boolean correct = check(i, map[sx][sy], sx, sy);
+
+                if(correct){
+                    TF = true;
+                    System.out.println(map[sx][sy]);
+                    System.out.println((sx + 1) + " " + (sy + 1));
+                    break;
+                }
+            }
+
+            if(TF){
+                break;
+            }
+        }
+
+        if(!TF){
+            System.out.println(0);
+        }
     }
 
-    public static boolean checkWin(int x, int y, int d, int color) {
-        int count = 1;
-        int nx = x + dx[d];
-        int ny = y + dy[d];
+    public static boolean check(int direction, int color, int x, int y){
+        int c = 1;
 
-        // 정방향으로 탐색 (같은 색의 돌이 연속되는지 확인)
-        while (isValid(nx, ny) && map[nx][ny] == color) {
-            count++;
-            nx += dx[d];
-            ny += dy[d];
+        int nx = x + dx[direction];
+        int ny = y + dy[direction];
+
+        while(isValid(nx, ny) && map[nx][ny] == color){
+            nx = nx + dx[direction];
+            ny = ny + dy[direction];
+            c++;
         }
 
-        // 반대 방향으로 탐색 (6개 이상 연속되는지 확인)
-        nx = x - dx[d];
-        ny = y - dy[d];
-        while (isValid(nx, ny) && map[nx][ny] == color) {
-            count++;
-            nx -= dx[d];
-            ny -= dy[d];
+        // 반대 방향으로도 탐색
+        nx = x - dx[direction];
+        ny = y - dy[direction];
+
+        while(isValid(nx, ny) && map[nx][ny] == color){
+            nx = nx - dx[direction];
+            ny = ny - dy[direction];
+            c = c + 1;
         }
 
-        // 정확히 5개여야 함 (6개 이상이면 false)
-        if (count != 5) return false;
-
-        // 6개 이상 연속된 경우를 체크 (반대 방향에 돌이 있는지 확인)
-        int prevX = x - dx[d];
-        int prevY = y - dy[d];
-        if (isValid(prevX, prevY) && map[prevX][prevY] == color) {
-            return false; // 6개 이상이면 false
+        if(c != 5){
+            return false;
         }
 
-        // 가장 왼쪽(세로 방향이면 가장 위쪽) 돌 찾기
-        int startX = x, startY = y;
-        while (isValid(startX - dx[d], startY - dy[d]) && map[startX - dx[d]][startY - dy[d]] == color) {
-            startX -= dx[d];
-            startY -= dy[d];
+        int prex = x - dx[direction];
+        int prey = y - dy[direction];
+
+        // 6개 이상 연속된 경우 false
+        if(isValid(prex, prey) && map[prex][prey] == color){
+            return false;
         }
+
+        // 가장 왼쪽(또는 위쪽) 돌 찾기
+        int startx = x;
+        int starty = y;
+
+//        while(isValid(startx - dx[direction], starty - dy[direction]) && map[startx - dx[direction]][starty - dy[direction]] == color){
+//            startx -= dx[direction];
+//            starty -= dy[direction];
+//        }
 
         return true;
     }
 
-    public static boolean isValid(int x, int y) {
+    public static boolean isValid(int x, int y){
         return x >= 0 && x < 19 && y >= 0 && y < 19;
     }
 }
