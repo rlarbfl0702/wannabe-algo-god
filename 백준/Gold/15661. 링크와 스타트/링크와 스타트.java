f6179e69293413d5486c505804d02a89ae0531f7
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-03-29 17:57:45 +0900

    [Gold V] Title: 링크와 스타트, Time: 1696 ms, Memory: 14828 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,92 +1,107 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
-public class Main{
-    static int[][] graph;
-    static int N;
-    static int[] team1;
-    static boolean[] visited;
-    static boolean[] visited2;
-    static boolean[] team;
-    static int[] teamCnt;
-    static int answer = Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
+public class Main {
+
+    public static int N;
+    public static int team[][];
+    public static int result;
+    public static boolean visited[];
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        team1 = new int[N/2];
-        visited = new boolean[N];
-        visited2 = new boolean[N];
-        teamCnt = new int[2];
-        graph = new int[N][N];
-        team = new boolean[N];
 
-        // 그래프 입력
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+        team = new int[N + 1][N + 1];
+
+        result = Integer.MAX_VALUE;
+
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j = 1; j <= N; j++){
+                int n = Integer.parseInt(st.nextToken());
+
+                team[i][j] = n;
             }
         }
 
-        for(int i=1; i<N/2+1; i++) {
-            recur(0, 0, i);
+        for(int i = 1; i < N; i++){
+            visited = new boolean[N + 1];
+            
+            DFS(0, i, 1);
         }
 
-        System.out.println(answer);
+        System.out.println(result);
+
     }
 
-    static void recur(int start, int depth, int max) {
-        // 팀 나누기
-        if(depth == max) {
-            int t = 0;
-            for(int i=0; i<N; i++) {
-                if(team[i]) {
-                    t++;
-                }
-            }
-            int[] team1 = new int[t];
-            int[] team2 = new int[N-t];
+    public static void DFS(int n, int d, int cnt){
+        if(n == d){
+            int start = 0;
+            int link = 0;
 
-            int t1 = 0;
-            int t2 = 0;
-            for(int i=0; i<N; i++){
-                if(team[i]) {
-                    team1[t1++] = i;
-                }
-                else {
-                    team2[t2++] = i;
-                }
-            }
-            int tmp = 0;
-
-            for(int i=0; i<t1-1; i++) {
-                for(int j=i+1; j<t1; j++) {
-                    tmp += graph[team1[i]][team1[j]] + graph[team1[j]][team1[i]];
+            // 방문한 적이 있다면 스타트팀이므로
+            // 스타트팀 능력치 계산에 사용
+            for(int i = 1; i <= N; i++){
+                for(int j = i; j <= N; j++){
+                    // 조합할 때, 똑같은 사람을 넣지 않기 위해서
+                    if(i != j){
+                        // 그 조합이 똑같지 않고
+                        // 두 사람 다 방문을 했다면 스타트팀이므로
+                        // 해당 배열의 값을 스타트팀 능력치에 더함
+                        if(visited[i] && visited[j]){
+                            start = start + team[i][j];
+                            start = start + team[j][i];
+                        }
+                    }
                 }
             }
 
-            for(int i=0; i<t2-1; i++) {
-                for(int j=i+1; j<t2; j++) {
-                    tmp -= graph[team2[i]][team2[j]] + graph[team2[j]][team2[i]];
+            // 방문한 적이 없다면 링크팀이므로
+            // 링크팀 능력치 계산에 사용
+            for(int i = 1; i <= N; i++){
+                for(int j = i; j <= N; j++){
+                    // 조합할 때, 똑같은 사람을 넣지 않기 위해서
+                    if(i != j){
+                        // 그 조합이 똑같지 않고
+                        // 두 사람 다 방문을 하지 않았다면 링크팀이므로
+                        // 해당 배열의 값을 링크팀 능력치에 더함
+                        if(!visited[i] && !visited[j]){
+                            link = link + team[i][j];
+                            link = link + team[j][i];
+                        }
+                    }
                 }
             }
 
+            int min = Math.abs(start - link);
 
-            answer = Math.min(answer, Math.abs(tmp));
+            if(min <= result){
+                result = min;
+
+//                System.out.print("스타트팀 ");
+//                for(int i = 1; i <= N; i++){
+//                    if(visited[i]){
+//                        System.out.print(i + " ");
+//                    }
+//                }
+//                System.out.println("결과값 " + start + " " + link);
+            }
+
             return;
         }
 
-        for(int i=start; i<N; i++) {
-            if(visited[i]) {
-                continue;
+        for(int i = cnt; i <= N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+
+                DFS(n + 1, d, i + 1);
+
+                visited[i] = false;
             }
-            team[i] = true;
-            visited[i] = true;
-            recur(i,depth+1, max);
-            team[i] = false;
-            visited[i] = false;
         }
     }
 }
