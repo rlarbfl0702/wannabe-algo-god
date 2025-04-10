5374368d3406366353061987c7b61976436689c2
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-04-10 21:01:07 +0900

    [Gold V] Title: A와 B, Time: 72 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,38 +1,96 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static String S, T;
-    static int result = 0;
+
+    public static String S;
+    public static String T;
+    public static int result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         S = br.readLine();
         T = br.readLine();
 
-        dfs(T);
+        result = 0;
+
+        // 영단어를 조합할 Deque
+        Deque<Character> dq = new ArrayDeque<>();
+
+        for(int i = 0; i < T.length(); i++){
+            Character c = T.charAt(i);
+
+            // 기존 문자열을 Deque에 순서대로 삽입
+            dq.addLast(c);
+        }
+
+        dfs(dq, true);
+
         System.out.println(result);
     }
 
-    // T에서 S로 줄여가는 역방향 DFS
-    static void dfs(String current) {
-        // 이미 성공했으면 리턴
-        if (result == 1) return;
-
-        // S와 길이가 같으면 비교
-        if (current.length() == S.length()) {
-            if (current.equals(S)) result = 1;
+    public static void dfs(Deque<Character> q, boolean fb){
+        if (result == 1){
             return;
         }
 
-        // A로 끝나면 A 제거하고 다음 탐색
-        if (current.endsWith("A")) {
-            dfs(current.substring(0, current.length() - 1));
+        // Deque의 크기와 S의 길이가 같다면
+        if (q.size() == S.length()) {
+            // Deque의 카피본
+            Deque<Character> copy = new ArrayDeque<>(q);
+            StringBuilder sb = new StringBuilder();
+
+            if(!fb){
+                // 뒤집힌 상태일 때 뒤에서부터 빼야함
+                for (int i = 0; i < S.length(); i++) {
+                    sb.append(copy.pollLast());
+                }
+            }
+            else{
+                // 정방향이면 앞에서부터 빼야함
+                for (int i = 0; i < S.length(); i++) {
+                    sb.append(copy.pollFirst());
+                }
+            }
+
+            // 만약 만들어진 문자열이 같다면 결과값을 1로 바꿈
+            if (sb.toString().equals(S)) {
+                result = 1;
+            }
+            return;
         }
 
-        // B로 끝나면 B 제거 + 뒤집기
-        if (current.endsWith("B")) {
-            StringBuilder sb = new StringBuilder(current.substring(0, current.length() - 1));
-            dfs(sb.reverse().toString());
+        // 뒤집혀있지 않다면
+        if(fb){
+            // A가 뒤에 있다면 제거
+            if (q.peekLast() == 'A') {
+                q.pollLast();
+                dfs(q, fb);
+                q.addLast('A');
+            }
+            // B가 앞에 있다면 제거 후, 뒤집기
+            if (q.peekLast() == 'B') {
+                q.pollLast();
+                dfs(q, !fb);
+                q.addLast('B');
+            }
+        }
+        // 뒤집혀 있고
+        else{
+            // A가 앞에 있다면 제거 (뒤집힌 상태이므로 뒤가 앞이기 때문에 앞에서 A를 제거)
+            if (q.peekFirst() == 'A') {
+                q.pollFirst();
+                dfs(q, fb);
+                q.addFirst('A');
+            }
+            // B가 뒤에 있다면 제거 후, 뒤집기
+            // (뒤집힌 상태이므로 뒤가 앞이기 때문에 뒤에서 B를 제거 후 뒤집음)
+            if (q.peekFirst() == 'B') {
+                q.pollFirst();
+                dfs(q, !fb);
+                q.addFirst('B');
+            }
         }
     }
 }
