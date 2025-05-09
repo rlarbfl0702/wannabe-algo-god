c50fe589a021678917d3de37a98f2cd4e9bacbdc
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-05-09 15:29:11 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N >= 620 && N <= 780){
+            System.out.println("Red");
+        }
+        else if(N >= 590 && N < 620){
+            System.out.println("Orange");
+        }
+        else if(N >= 570 && N < 590){
+            System.out.println("Yellow");
+        }
+        else if(N >= 495 && N < 570){
+            System.out.println("Green");
+        }
+        else if(N >= 450 && N < 495){
+            System.out.println("Blue");
+        }
+        else if(N >= 425 && N < 450){
+            System.out.println("Indigo");
+        }
+        else if(N >= 380 && N < 425){
+            System.out.println("Violet");
+        }
+
+    }
+}
