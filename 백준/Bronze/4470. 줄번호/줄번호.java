b4a8bf2bc16fb58aad0f04be74065547686ebadc
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-05-10 23:56:10 +0900

    [Bronze IV] Title: 줄번호, Time: 96 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            sb.append(i + 1).append('.').append(' ').append(br.readLine()).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+}
