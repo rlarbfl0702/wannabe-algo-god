8c454905b7810455bf882b17950679129fe91205
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-05-11 23:05:24 +0900

    [Bronze V] Title: Rats, Time: 64 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n3 = Integer.parseInt(st.nextToken());
+
+        int result = ((n1 + 1) * (n2 + 1) / (n3 + 1)) - 1;
+
+        System.out.println(result);
+    }
+}
