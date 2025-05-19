eca056bfa30508addd656c5f3a2820430833b407
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-05-19 17:18:16 +0900

    [Bronze IV] Title: 영수증, Time: 64 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int total = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < 9; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            total = total - n;
+        }
+
+        System.out.println(total);
+    }
+}
