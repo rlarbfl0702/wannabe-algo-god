c74f1cf74067ab27905a8a8351483d5afdcf99c8
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-07-08 18:00:33 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(N + M, N - M));
+        System.out.println(Math.min(N - M, N + M));
+    }
+}
