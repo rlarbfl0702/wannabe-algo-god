e9c72c721da51e35e9b16eb90a1e20a2760f0b18
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-07-11 22:02:22 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 64 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if((A + C) < B){
+            System.out.println(B);
+        }
+        else{
+            System.out.println(A + C);
+        }
+
+    }
+}
