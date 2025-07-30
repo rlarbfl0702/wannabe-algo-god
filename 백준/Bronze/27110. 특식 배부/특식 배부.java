39440084737a08734e987187ce0161417e4807b2
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-07-30 23:01:23 +0900

    [Bronze IV] Title: 특식 배부, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int result = Math.min(N, A) + Math.min(N, B) + Math.min(N, C);
+
+        System.out.println(result);
+    }
+}
