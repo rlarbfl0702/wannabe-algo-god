a21abe485e24d372261d9d49ed65996021a5034f
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-08-28 14:58:33 +0900

    [Silver V] Title: LCM, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int test = 0; test < T; test++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            long N = Integer.parseInt(st.nextToken());
+            long M = Integer.parseInt(st.nextToken());
+
+
+            long r = gcd(N, M);
+
+            long result = (N * M) / r;
+
+            sb.append(result + "\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    // 유클리드 호제법
+    public static long gcd(long A, long B) {
+        if (B == 0) {
+            return A;
+        }
+
+        return gcd(B, A % B);
+    }
+}
