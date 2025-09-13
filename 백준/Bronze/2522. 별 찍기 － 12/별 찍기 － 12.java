df0e9e983d351430b68dbbe700e63f89dba9841f
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-09-13 15:53:20 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 72 ms, Memory: 11724 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int j = 1; j <= N; j++){
+            for(int k = 0; k < N - j; k++){
+                sb.append(" ");
+            }
+
+            for(int k = 0; k < j; k++){
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        for(int j = N; j >= 2; j--){
+            for(int k = 0; k < N - j + 1; k++){
+                sb.append(" ");
+            }
+
+            for(int k = 1; k < j; k++){
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+
+        System.out.println(sb);
+    }
+}
