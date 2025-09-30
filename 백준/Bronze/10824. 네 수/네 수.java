ba95add24765f1015358a8d70cf402942b01ea4e
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-09-30 19:18:43 +0900

    [Bronze III] Title: 네 수, Time: 68 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        String AB = A + "" + B;
+        String CD = C + "" + D;
+
+        BigInteger result = new BigInteger(AB).add(new BigInteger(CD));
+
+        System.out.println(result);
+    }
+}
