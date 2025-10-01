16505c89af898f53e086f406a6507b89635a9d19
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-10-01 22:15:36 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int result = Math.min(Math.min(N / 2, M), (N + M - K) / 3);
+
+        System.out.println(result);
+
+    }
+}
