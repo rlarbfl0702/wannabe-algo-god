98906819e34bc18ab496d6a5e2c567a72a724e36
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-11-23 19:12:24 +0900

    [Bronze IV] Title: Communication Channels, Time: 64 ms, Memory: 11660 KB -BaekjoonHub

---
+++ 백준/Bronze/11121. Communication Channels/Communication Channels.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String T = st.nextToken();
+            String T2 = st.nextToken();
+
+            if(!T.equals(T2)){
+                sb.append("ERROR" + "\n");
+            }
+            else{
+                sb.append("OK" + "\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
