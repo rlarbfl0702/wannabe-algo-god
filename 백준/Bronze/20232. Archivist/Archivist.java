00b8baf4748b46254cef5a7ef193ca0ba0eb6b6b
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2025-12-11 18:02:51 +0900

    [Bronze IV] Title: Archivist, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N == 1995){
+            System.out.println("ITMO");
+        }
+        else if(N == 1996){
+            System.out.println("SPbSU");
+        }
+        else if(N == 1997){
+            System.out.println("SPbSU");
+        }
+        else if(N == 1998){
+            System.out.println("ITMO");
+        }
+        else if(N == 1999){
+            System.out.println("ITMO");
+        }
+        else if(N == 2000){
+            System.out.println("SPbSU");
+        }
+        else if(N == 2001){
+            System.out.println("ITMO");
+        }
+        else if(N == 2002){
+            System.out.println("ITMO");
+        }
+        else if(N == 2003){
+            System.out.println("ITMO");
+        }
+        else if(N == 2004){
+            System.out.println("ITMO");
+        }
+        else if(N == 2005){
+            System.out.println("ITMO");
+        }
+        else if(N == 2006){
+            System.out.println("PetrSU, ITMO");
+        }
+        else if(N == 2007){
+            System.out.println("SPbSU");
+        }
+        else if(N == 2008){
+            System.out.println("SPbSU");
+        }
+        else if(N == 2009){
+            System.out.println("ITMO");
+        }
+        else if(N == 2010){
+            System.out.println("ITMO");
+        }
+        else if(N == 2011){
+            System.out.println("ITMO");
+        }
+        else if(N == 2012){
+            System.out.println("ITMO");
+        }
+        else if(N == 2013){
+            System.out.println("SPbSU");
+        }
+        else if(N == 2014){
+            System.out.println("ITMO");
+        }
+        else if(N == 2015){
+            System.out.println("ITMO");
+        }
+        else if(N == 2016){
+            System.out.println("ITMO");
+        }
+        else if(N == 2017){
+            System.out.println("ITMO");
+        }
+        else if(N == 2018){
+            System.out.println("SPbSU");
+        }
+        else if(N == 2019){
+            System.out.println("ITMO");
+        }
+    }
+}
