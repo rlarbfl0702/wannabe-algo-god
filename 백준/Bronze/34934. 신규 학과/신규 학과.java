a4dd954390493eb0cc87a0b290bc8e2457d7b6c7
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2026-01-07 19:10:19 +0900

    [Bronze IV] Title: 신규 학과, Time: 68 ms, Memory: 11776 KB -BaekjoonHub

---
+++ 백준/Bronze/34934. 신규 학과/신규 학과.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String s = st.nextToken();
+            int y = Integer.parseInt(st.nextToken());
+
+            if(y == 2026){
+                System.out.println(s);
+            }
+        }
+    }
+}
