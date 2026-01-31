b10dc56561b59be811587c9ab0eff788e482d510
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2026-01-31 15:32:18 +0900

    [Bronze I] Title: 2007년, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+        int totalDays = y;
+        for (int i = 1; i < x; i++) {
+            totalDays += months[i];
+        }
+
+        System.out.println(days[totalDays % 7]);
+    }
+}
