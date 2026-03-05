f4cd5dc91f517bd6139530c142599f272fb78c42
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2026-03-05 16:13:45 +0900

    [Bronze V] Title: Flatter Land, Time: 68 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/34424. Flatter Land/Flatter Land.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+
+        System.out.println((N - 1) * T);
+    }
+}
