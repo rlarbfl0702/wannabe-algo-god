1e6086303f294e17a6913eb7008b33971f64bc60
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2026-03-06 15:02:13 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 72 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        System.out.println((A * B) + C);
+    }
+}
