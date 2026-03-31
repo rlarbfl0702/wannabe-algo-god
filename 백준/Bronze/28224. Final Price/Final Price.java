822aeb5e4444e67cf98bcef1b921d7f6d23747ff
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2026-03-31 15:27:16 +0900

    [Bronze IV] Title: Final Price, Time: 68 ms, Memory: 11740 KB -BaekjoonHub

---
+++ 백준/Bronze/28224. Final Price/Final Price.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for(int i = 0; i < T; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            result = result + n;
+        }
+
+        System.out.println(result);
+    }
+}
