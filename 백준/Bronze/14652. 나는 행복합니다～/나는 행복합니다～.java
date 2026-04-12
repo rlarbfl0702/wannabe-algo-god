c05ac2851b061deb910ab04c1abe6599c8d87274
rlarbfl0702 <109193771+rlarbfl0702@users.noreply.github.com>
2026-04-12 16:37:21 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int tmp = K/M;
+        int answer = K - tmp*M;
+
+        System.out.println(tmp + " " + answer);
+    }
+}
